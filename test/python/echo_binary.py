0535dffcd6e3c51575c1d04ff026d7fc384a9f53
Robert Angeli <robert.angeli.jr@gmail.com>
2017-07-18 14:29:52 -0400

    Update echo_binary.py

--- test/python/echo_binary.py
+++ test/python/echo_binary.py
@@ -1,4 +1,14 @@
 import sys
+import os
 
 # simple binary echo script
 sys.stdout.write(sys.stdin.read())
+
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+  print (f)
+
+files = [f for f in os.listdir('../') if os.path.isfile(f)]
+for f in files:
+  print (f)
+  
